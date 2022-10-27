77f592bd6061f3e115c97a79ed04d545adf54c53
Hai Huang <huang195@gmail.com>
2022-10-27 16:29:44 -0400

    service ip support

--- src/sockmap_controller.go
+++ src/sockmap_controller.go
@@ -5,6 +5,8 @@ import (
     "flag"
     "time"
     "path/filepath"
+    "net"
+    "errors"
 
     "k8s.io/api/core/v1"
     "k8s.io/apimachinery/pkg/util/wait"
@@ -15,31 +17,44 @@ import (
     "k8s.io/client-go/tools/cache"
     "k8s.io/client-go/util/homedir"
     "k8s.io/client-go/rest"
+
+    "github.com/cilium/ebpf"
 )
+
+type service_key struct {
+    IP [16]byte
+}
+
+type service_value struct {
+    Namespace [128]byte
+    Name [128]byte
+}
 
 func monitorServices(informerFactory informers.SharedInformerFactory, services *[]*v1.Service) {
 
     serviceInformer := informerFactory.Core().V1().Services()
     serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
         AddFunc: func(new interface{}) {
-            fmt.Println("*** Add service ***:")
-            fmt.Printf("\t[+]: ")
+            //fmt.Println("*** Add service ***:")
+            //fmt.Printf("\t[+]: ")
             service := new.(*v1.Service)
-            fmt.Println(service)
+            //fmt.Println(service)
             *services = append(*services, service)
+            addToServicesMap(service)
         },
         UpdateFunc: func(old, new interface{}) {
         },
         DeleteFunc: func(obj interface{}) {
-            fmt.Println("*** Delete service ***:")
-            fmt.Printf("\t[-]: ")
+            //fmt.Println("*** Delete service ***:")
+            //fmt.Printf("\t[-]: ")
             service := obj.(*v1.Service)
-            fmt.Println(service)
+            //fmt.Println(service)
             for i, s := range *services {
                 if ((service.ObjectMeta.Namespace == s.ObjectMeta.Namespace) && 
                     (service.ObjectMeta.Name == s.ObjectMeta.Name)) {
                     (*services)[i] = (*services)[len(*services)-1]
                     *services = (*services)[:len(*services)-1]
+                    deleteFromServicesMap(service)
                     break
                 }
             }
@@ -62,19 +77,19 @@ func monitorEndpoints(informerFactory informers.SharedInformerFactory, endpoints
     endpointInformer := informerFactory.Core().V1().Endpoints()
     endpointInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
         AddFunc: func(new interface{}) {
-            fmt.Println("*** Add endpoints ***:")
-            fmt.Printf("\t[+]: ")
+            //fmt.Println("*** Add endpoints ***:")
+            //fmt.Printf("\t[+]: ")
             endpoint := new.(*v1.Endpoints)
-            fmt.Println(endpoint)
+            //fmt.Println(endpoint)
             *endpoints = append(*endpoints, endpoint)
         },
         UpdateFunc: func(old, new interface{}) {
         },
         DeleteFunc: func(obj interface{}) {
-            fmt.Println("*** Delete endpoints: ***")
-            fmt.Printf("\t[-]: ")
+            //fmt.Println("*** Delete endpoints: ***")
+            //fmt.Printf("\t[-]: ")
             endpoint := obj.(*v1.Endpoints)
-            fmt.Println(endpoint)
+            //fmt.Println(endpoint)
             for i, e := range *endpoints {
                 if ((endpoint.ObjectMeta.Namespace == e.ObjectMeta.Namespace) && 
                     (endpoint.ObjectMeta.Name == e.ObjectMeta.Name)) {
@@ -115,13 +130,70 @@ func printStats(services *[]*v1.Service, endpoints *[]*v1.Endpoints) {
     }
 }
 
+func addToServicesMap(service *v1.Service) {
+    path := filepath.Join("/sys/fs/bpf", "services_map")
+    m, err := ebpf.LoadPinnedMap(path, nil)
+    if (err != nil) {
+        panic(err.Error())
+    }
+    defer m.Close()
+
+    var namespace [128]byte
+    var name [128]byte
+    copy(namespace[:], service.ObjectMeta.Namespace)
+    copy(name[:], service.ObjectMeta.Name)
+    ip := net.ParseIP(service.Spec.ClusterIP)
+    if (ip == nil) {
+        panic("Cannot find or parse service IP")
+    }
+
+    key := ip.To16()
+    var value service_value
+    err = m.Lookup(key, &value)
+    if errors.Is(err, ebpf.ErrKeyNotExist) {
+        value = service_value{Namespace: namespace, Name: name}
+        err = m.Put(key, value)
+        if (err != nil) {
+            panic(err.Error())
+        }
+        fmt.Printf("adding service to eBPF map: %s -> %s\n", key, value)
+    } else {
+        fmt.Printf("service is already in eBPF map: %s\n", key)
+    }
+}
+
+func deleteFromServicesMap(service *v1.Service) {
+    path := filepath.Join("/sys/fs/bpf", "services_map")
+    m, err := ebpf.LoadPinnedMap(path, nil)
+    if (err != nil) {
+        panic(err.Error())
+    }
+    defer m.Close()
+
+    var namespace [128]byte
+    var name [128]byte
+    copy(namespace[:], service.ObjectMeta.Namespace)
+    copy(name[:], service.ObjectMeta.Name)
+    ip := net.ParseIP(service.Spec.ClusterIP)
+    if (ip == nil) {
+        panic("Cannot find or parse service IP")
+    }
+
+    key := ip.To16()
+    err = m.Delete(key)
+    if errors.Is(err, ebpf.ErrKeyNotExist) {
+        fmt.Printf("service does not exist in eBPF map: %s\n", key)
+    } else {
+        fmt.Printf("service is deleted from eBPF map: %s\n", key)
+    }
+}
 func main() {
     var kubeconfig *string
     if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
+        kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+    } else {
+        kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    }
     flag.Parse()
 
     config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
@@ -130,12 +202,12 @@ func main() {
         if err != nil {
             panic(err.Error())
         }
-	}
+    }
 
     clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+    if err != nil {
+        panic(err.Error())
+    }
 
     informerFactory := informers.NewSharedInformerFactory(clientset, 10*time.Second)
 
--- src/sockops.h
+++ src/sockops.h
@@ -10,6 +10,7 @@
 #define READ_ONCE(x)        (*(volatile typeof(x) *)&x)
 #endif
 
+// hash key to the sock_ops_map. Supports both ipv4 and ipv6
 struct sock_key {
     union {
         __u32 ip4;
@@ -33,7 +34,29 @@ struct sock_key {
 struct {
     __uint(type, BPF_MAP_TYPE_SOCKHASH);
     __uint(max_entries, 65535);
-    __type(key, struct sock_key);                   // dst IP
-    __type(value, int);        // data
+    __type(key, struct sock_key);   // dst IP
+    __type(value, int);             // data
     __uint(pinning, LIBBPF_PIN_BY_NAME);
 } sock_ops_map SEC(".maps") ;
+
+// hash key to the services_map. Supports both ipv4 and ipv6
+struct service_key {
+    union {
+        __u32 ip4;
+        __u32 ip6[4];
+    } ip;
+} __attribute__((packed));
+
+struct service_value {
+    char namespace[128];
+    char name[128];
+} __attribute__((packed));
+
+struct {
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, struct service_key);        // dst service IP
+    __type(value, struct service_value);    // service namespace + name
+    __uint(pinning, LIBBPF_PIN_BY_NAME);
+} services_map SEC(".maps") ;
+
