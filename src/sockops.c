69946cb3163ba535e648c81d20684f0a60f3c808
Hai Huang <huang195@gmail.com>
2022-11-23 12:43:32 -0500

    updated README and disables debug

--- src/sockops.c
+++ src/sockops.c
@@ -41,8 +41,8 @@ void bpf_sock_ops_ipv4(struct bpf_sock_ops *skops)
 
     // insert the source socket in the sock_ops_map
     int ret = bpf_sock_hash_update(skops, &sock_ops_map, &key, BPF_NOEXIST);
-    bpf_printk("<<< ipv4 op = %d, port %d --> %d\n",
-        skops->op, skops->local_port, bpf_ntohl(skops->remote_port));
+    //bpf_printk("<<< ipv4 op = %d, port %d --> %d\n",
+        //skops->op, skops->local_port, bpf_ntohl(skops->remote_port));
     if (ret != 0) {
         bpf_printk("FAILED: bpf_sock_hash_update ret: %d\n", ret);
     }
